71deb491ee433ee677e31e543be069d091d6ee71
AImetricsIO <aimetricsio@gmail.com>
2024-06-12 10:00:36 +0200

    Add files via upload

---
+++ mi_libreria.py
@@ -0,0 +1,13 @@
+# mi_libreria.py
+
+def funcion1(param):
+    return f'Función 1 dice: {param}'
+
+def funcion2(param):
+    return f'Función 2 dice: {param}'
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
