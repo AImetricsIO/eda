2e198e13df03a6002614c601172511df70b245cd
AImetrics <aimetricsio@gmail.com>
2024-06-12 11:09:50 +0200

    Update edabasic.py

--- edabasic.py
+++ edabasic.py
@@ -1,14 +1,16 @@
-# mi_libreria.py
+# To be used in google collab notebooks
 
-def funcion1(param):
-    return f'Función 1 dice: {param}'
+# Intraday data shows NaN in the last volume row. We'll change it with an average
+def fill_na_with_previous_mean(df, window=78):
+    for i in range(len(df)):
+        if pd.isna(df.loc[i, 'volume']):
+           start = max(0, i - window)
+           subset = df.loc[start:i, 'volume']
+           mean_value = subset.mean() if not subset.empty else 0
+           df.loc[i, 'volume'] = mean_value
+    return df
 
-def funcion2(param):
-    return f'Función 2 dice: {param}'
-
-def sumar(a, b):
-    return a + b
-
+# Daily prices are adjusted with splits and other corporte events
 def adjust_close(df):
     # Calculate the adjusted_close ratio
     df['ratio'] = df['close'] / df['adjusted_close']
